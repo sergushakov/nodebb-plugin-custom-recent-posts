59e3c975a4a146784d553be2e527feee46946228
Serg Ushakov <sergushakov.public@gmail.com>
2015-07-26 18:30:27 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Custom recent posts
+## Custom recent posts
+This NodeBB plugin just adds the setting for changing a quantity of last posts that showing on the home page.
+Technically the plugin modifies categories.posts array in the home page api call
+
+## Install
+npm install nodebb-plugin-custom-recent-posts
+
---
+++ plugin.js
@@ -0,0 +1,54 @@
+/* global module,Promise */
+(function (module){
+    'use strict';
+    var plugin = {},
+        meta = module.parent.require("./meta"),
+        path = module.parent.require('path'),
+		    nconf = module.parent.require('nconf'),
+        Categories = module.parent.require("./categories.js")
+    ;
+    plugin.init = function(params, callback) {
+		    var app = params.router,
+			      middleware = params.middleware;
+		    app.get('/admin/plugins/custom-recent-posts', middleware.admin.buildHeader, renderAdmin);
+		    app.get('/api/admin/plugins/custom-recent-posts', renderAdmin);
+		    callback();
+	  };
+    plugin.preinit = function(params, callback) {
+		    nconf.set('base_templates_path', path.join(nconf.get('themes_path'), 'nodebb-theme-vanilla/templates'));
+
+		    callback();
+	  };
+    plugin.addAdminNavigation = function(header, callback) {
+		    header.plugins.push({
+			      route: '/plugins/custom-recent-posts',
+			      icon: 'fa-paint-brush',
+			      name: 'Custom previews'
+		    });
+		    callback(null, header);
+	  };
+    plugin.getConfig = function(config, callback) {
+        config.previewsCount = parseInt(meta.config.previewsCount, 10);
+		    callback(null, config);
+	  };
+    plugin.getCategories = function(obj,callback){
+        Promise.all(obj.templateData.categories.map(function(x){
+            return new Promise(function(resolve,reject){
+                Categories.getRecentReplies(x.cid,obj.req.uid,meta.config.previewsCount || 1,function(err,posts){
+                    if(err) reject(err);
+                    x.posts = posts;
+                    resolve(x);
+                });
+            });
+        })).then(function(data){
+            obj.templateData.categories = data;
+            callback(null,obj);
+        }).catch(function(err){
+            callback(err,obj);
+        });
+    };
+    function renderAdmin(req, res) {
+		    res.render('admin/plugins/custom-recent-posts', {});
+	  }
+    module.exports = plugin;
+})(module);
