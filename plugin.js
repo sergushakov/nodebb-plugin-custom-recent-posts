050434b2a95420656a0552cac7ba6c14c63c9624
Sergey Ushakov <sergushakov.public@gmail.com>
2015-08-23 11:53:23 +0300

    Cleanup formatting.

--- plugin.js
+++ plugin.js
@@ -4,33 +4,38 @@
     var plugin = {},
         meta = module.parent.require("./meta"),
         path = module.parent.require('path'),
-		    nconf = module.parent.require('nconf'),
+        nconf = module.parent.require('nconf'),
         Categories = module.parent.require("./categories.js")
     ;
-    plugin.init = function(params, callback) {
-		    var app = params.router,
-			      middleware = params.middleware;
-		    app.get('/admin/plugins/custom-recent-posts', middleware.admin.buildHeader, renderAdmin);
-		    app.get('/api/admin/plugins/custom-recent-posts', renderAdmin);
-		    callback();
-	  };
-    plugin.preinit = function(params, callback) {
-		    nconf.set('base_templates_path', path.join(nconf.get('themes_path'), 'nodebb-theme-vanilla/templates'));
 
-		    callback();
-	  };
+    plugin.init = function(params, callback) {
+        var app = params.router,
+            middleware = params.middleware
+        ;
+        app.get('/admin/plugins/custom-recent-posts', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/plugins/custom-recent-posts', renderAdmin);
+        callback();
+    };
+
+    plugin.preinit = function(params, callback) {
+        nconf.set('base_templates_path', path.join(nconf.get('themes_path'), 'nodebb-theme-vanilla/templates'));
+        callback();
+    };
+
     plugin.addAdminNavigation = function(header, callback) {
-		    header.plugins.push({
-			      route: '/plugins/custom-recent-posts',
-			      icon: 'fa-paint-brush',
-			      name: 'Custom previews'
-		    });
-		    callback(null, header);
-	  };
+        header.plugins.push({
+              route: '/plugins/custom-recent-posts',
+              icon: 'fa-list-ol',
+              name: 'Custom previews'
+        });
+        callback(null, header);
+    };
+
     plugin.getConfig = function(config, callback) {
         config.previewsCount = parseInt(meta.config.previewsCount, 10);
-		    callback(null, config);
-	  };
+        callback(null, config);
+    };
+
     plugin.getCategories = function(obj,callback){
         Promise.all(obj.templateData.categories.map(function(x){
             return new Promise(function(resolve,reject){
@@ -47,8 +52,11 @@
             callback(err,obj);
         });
     };
+
     function renderAdmin(req, res) {
-		    res.render('admin/plugins/custom-recent-posts', {});
+        res.render('admin/plugins/custom-recent-posts', {});
 	  }
+
     module.exports = plugin;
+
 })(module);
