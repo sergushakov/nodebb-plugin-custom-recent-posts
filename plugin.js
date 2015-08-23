cb679f3c9a7f417e8ab0fad619ad8e9400ba80f0
Sergey Ushakov <sergushakov.public@gmail.com>
2015-08-23 12:14:29 +0300

    Added subcats posts previews.

--- plugin.js
+++ plugin.js
@@ -36,22 +36,99 @@
         callback(null, config);
     };
 
-    plugin.getCategories = function(obj,callback){
+    plugin.buildCategories = function(obj,callback){
+
         Promise.all(obj.templateData.categories.map(function(x){
-            return new Promise(function(resolve,reject){
-                Categories.getRecentReplies(x.cid,obj.req.uid,meta.config.previewsCount || 1,function(err,posts){
-                    if(err) reject(err);
-                    x.posts = posts;
-                    resolve(x);
-                });
-            });
+            return prepareCategory(x,obj.req.uid);
         })).then(function(data){
             obj.templateData.categories = data;
-            callback(null,obj);
+            callback(false,obj);
         }).catch(function(err){
             callback(err,obj);
         });
     };
+
+    plugin.buildCategory = function(obj,callback){
+        if(obj.templateData.cid === meta.config.blogCategoryId){
+            obj.templateData.isBlog = true;
+        }
+        callback(false,obj);
+    };
+
+    plugin.getCategory = function(obj,callback){
+        Promise
+            .all(obj.category.children.map(function(x){
+                if(!x.children) x.children = [];
+                return prepareCategory(x,obj.uid);
+            }))
+            .then(function(children){
+                obj.category.children = children;
+                callback(null,obj);
+            })
+            .catch(function(err){
+                callback(err,obj);
+            })
+        ;
+    };
+
+    plugin.getTopics = function(obj,callback){
+        Promise.all(obj.topics.map(function(x){
+            return new Promise(function(resolve,reject){
+                Topics.getMainPost(x.tid,obj.uid,function(err,data){
+                    if(err) reject(err);
+                    x.content = data.content;
+                    Posts.getPostField(x.pid,"test",function(err,rsp){
+                        x.extra = rsp;
+                        resolve(x);
+                    });
+                });
+            });
+        })).then(function(data){
+            obj.topics = data;
+            callback(false,obj);
+        });
+    };
+
+    function prepareCategory(x,uid){
+        return new Promise(function(resolve,reject){
+            Categories.getRecentReplies(x.cid,uid,meta.config.previewsCount || 1,function(err,data){
+                if(err) reject(err);
+                Promise
+                    .all(x.children.map(function(y){
+                        return new Promise(function(res,rej){
+                            Categories.getRecentReplies(y.cid,uid,meta.config.previewsCount || 1,function(err,data_){
+                                if(err) rej(err);
+                                res(data_);
+                            });
+                        });
+                    }))
+                    .then(function(children){
+                        x.tP = data;
+                        x.cP = children;
+                        var posts = [];
+                        if(data.length === 0 && children.length === 0){
+                            posts = [];
+                        } else if (data.length === 0 && children.length !== 0){
+                            posts = children[0];
+                        } else if (data.length !== 0 && children.length === 0){
+                            posts = data;
+                        } else {
+                            posts = data.concat(children[0]);
+                        }
+
+                        x.posts = posts.sort(function(l,r){
+                            return r.timestamp - l.timestamp;
+                        }).slice(0,meta.config.previewsCount || 1);
+                        if(x.posts.length === 1 && x.posts[0] === null) x.posts = [];
+                        resolve(x);
+                    })
+                    .catch(function(err){
+                        reject(err);
+                    })
+                ;
+            });
+        });
+    }
 
     function renderAdmin(req, res) {
         res.render('admin/plugins/custom-recent-posts', {});
